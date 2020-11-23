607b92d6ef38695b7df30a603f08ad3f0e9b38c8
Bogdan Ripa <bogdanripa@gmail.com>
2020-11-23 18:31:19 +0200

    resilience

--- client/index.js
+++ client/index.js
@@ -1,13 +1,12 @@
 const io = require('socket.io-client');
 const screenshot = require('screenshot-desktop')
 const socket = io("wss://YOUR-APP-DETAILS.appspot.com/", {
-  reconnectionDelayMax: 1000
+	reconnectionDelayMax: 1000
 });
 const os = require('os');
 const resizeImg = require('resize-img');
 
 const id = os.hostname().replace(/[^a-z0-9]/ig, '-');
-console.log("Init: " + id);
 
 var sHbTo = 0;
 var hbTo = 0;
@@ -15,45 +14,51 @@ function heartbeat() {
 	socket.emit('heartbeat', id);
 }
 
+function log(msg) {
+	console.log('[' + new Date().toUTCString() + '] ' + msg);
+}
+
+log("Init: " + id);
+
 socket.on('connect', () => {
-	console.log("Connected");
+	log("Connected");
 
 	heartbeat();
 	if (hbTo) clearInterval(hbTo);
-	setInterval(heartbeat, 60 * 1000);
+	hbTo = setInterval(heartbeat, 60 * 1000);
 });
 
 socket.on('disconnect', (reason) => {
-	console.log("Disconnected: " + reason);
+	log("Disconnected: " + reason);
 	if (hbTo) clearInterval(hbTo);
-	socket.connect();
+	//socket.connect();
 });
 
 socket.on('connect_error', (error) => {
-	console.log("Error: " + error);
+	log("Error: " + error);
 	if (hbTo) clearInterval(hbTo);
-	socket.connect();
+	//socket.connect();
 });
 
 socket.on('get', (data) => {
 	if (data == id) {
-		console.log("Screenshot requested");
+		log("Screenshot requested");
 		screenshot().then((img) => {
 			resizeImg(img, {width: 1024}).then((img) => {
 				var base64data = Buffer.from(img).toString('base64');
 				socket.emit('ss', data + ' <img src="data:image/png;base64,' + base64data + '" width="100%"/>');
 			});
 		}).catch((error) => {
-			console.log("Screenshot error: " + error);
+			log("Screenshot error: " + error);
 		});
 	}
 });
 
 socket.on('heartbeat', (data) => {
-	console.log("Server heartbeat")
+	log("Server heartbeat")
 	if (sHbTo) clearTimeout(sHbTo);
 	sHbTo = setTimeout(function() {
-		console.log("No server heartbeat... disconnecting")
+		log("No server heartbeat... disconnecting")
 		sHbTo = 0;
 		socket.close();
 	}, 5 * 60 * 1000);
