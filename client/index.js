aa991ea25d9b339b57a6cb28a9336be875148025
Bogdan Ripa <bogdanripa@gmail.com>
2021-12-01 14:06:56 +0200

    added callback on process kill

--- client/index.js
+++ client/index.js
@@ -1,4 +1,5 @@
 SOCKET_URL = "wss://YOUR-APP.appspot.com/";
+CALLBACK_URL = "https://maker.ifttt.com/trigger/Closed/with/key/YOUR-KEY?value1={what}&value2={app}&value3={where}";
 
 var lastLog = '';
 log = function(msg) {
