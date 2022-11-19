bc09e8b55db7c9eca13732eda09e3cba06bb6dca
qilif <ripafilip@gmail.com>
2022-11-19 14:27:57 +0200

    probleme

--- client/index.js
+++ client/index.js
@@ -1,5 +1,5 @@
-SOCKET_URL = "wss://YOUR-APP.appspot.com/";
-CALLBACK_URL = "https://maker.ifttt.com/trigger/Closed/with/key/YOUR-KEY?value1={what}&value2={app}&value3={where}";
+SOCKET_URL = "wss://ssproxy2-327407.ey.r.appspot.com/";
+CALLBACK_URL = "https://maker.ifttt.com/trigger/Closed/with/key/bWKT4o3AQ19MMPkx_7yMUf?value1={what}&value2={app}&value3={where}";
 
 var lastLog = '';
 log = function(msg) {
